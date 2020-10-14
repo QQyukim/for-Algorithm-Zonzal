667819ca262f301cfe93f3eae09b80fa5effeea5
QQyukim <namgyu9696@gmail.com>
2020-10-14 18:10:41 +0900

    nong님 코칭 코드를 주석처리하여 추가

--- BOJ/11651-좌표 정렬하기 2.py
+++ BOJ/11651-좌표 정렬하기 2.py
@@ -10,3 +10,19 @@ dots.sort(key = lambda x : (x[1], x[0]))
 
 for dot in dots:
     print(dot[0], dot[1])
+
+'''
+# nong님 풀이
+
+n = int(input())
+arr = []
+
+for i in range(n):
+    x, y = map(int, input().split())
+    arr.append((y, x))
+
+arr.sort()
+
+for (y, x) in arr:
+    print(x, y)
+'''
