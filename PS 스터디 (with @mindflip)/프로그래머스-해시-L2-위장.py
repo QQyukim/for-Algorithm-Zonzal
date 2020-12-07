0ea8c05e3d1748d0744b887ac45c014f1ebc5a44
QQyukim <namgyu9696@gmail.com>
2020-12-07 22:53:35 +0900

    2020/12/07 문제풀이 (자력)

--- PS 스터디 (with @mindflip)/프로그래머스-해시-L2-위장.py
+++ PS 스터디 (with @mindflip)/프로그래머스-해시-L2-위장.py
@@ -1,26 +1,25 @@
-# 파이썬 딕셔너리 쌍 추가하기 => 활용!
-# 풀이 참고 (https://eda-ai-lab.tistory.com/472)
-
 def solution(clothes):
-    answer = {}
+    dic = {}
+    answer = 1
 
-    for ele in clothes:
-        if ele[1] in answer:
-            answer[ele[1]] += 1
+    for el in clothes:
+        name = el[0]
+        kind = el[1]
+
+        if kind in dic:
+            dic[kind] += 1
         else:
-            answers[ele[1]] = 1
+            dic[kind] = 1
 
-    # {'hg': 2, 'ew': 1}
-    
-    cnt = 1
-    for value in answer.values():
-        cnt *= (value + 1)
-    
-    return cnt - 1
+    for v in dic.values():
+        answer *= v + 1    
 
-    # 한 종류당 입지 않은 것까지 모두 고려 (집합 생각)
-    # 종류가 같은 것끼리 묶어서 (ex) (hg + 1) * (ew + 1) - 1 로 계산
-    # 이 때 '+ 1'은 해당 옷을 안 걸칠 때를 뜻함
+    answer = answer - 1
+    return answer
+
+# 한 종류당 입지 않은 것까지 모두 고려 (집합 생각)
+# 종류가 같은 것끼리 묶어서 (ex) (hg + 1) * (ew + 1) - 1 로 계산
+# 이 때 '+ 1'은 해당 옷을 안 걸칠 때를 뜻함
 
 clothes1 = [['y_h', 'hg'], ["b_s", "ew"], ['g_t', 'hg']]
 clothes2 = [['c_m', 'f'], ["b_s", "f"], ['s_m', 'f']]
